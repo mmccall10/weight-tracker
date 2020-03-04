73def2bba7a91ef35e99da2ce3426af62b6d24d3
Mike McCall <mikemccall10@gmail.com>
2020-03-04 05:59:32 -0800

    add current weight

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @max_weight = @user.entries.maximum(:weight)
     @min_weight = @user.entries.minimum(:weight)
+    @current_weight = @user.entries.pluck(:weight).last
   end
 end
