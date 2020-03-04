d4ef0d08f3e87cc9739231eaa59945c03905c61a
Mike McCall <mikemccall10@gmail.com>
2020-03-04 06:08:46 -0800

    add average

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
     @max_weight = @user.entries.maximum(:weight)
     @min_weight = @user.entries.minimum(:weight)
     @current_weight = @user.entries.pluck(:weight).last
+    @seven_day_average = @user.entries.where("created_at > ?", 7.days.ago).average(:weight)
+    @thirty_day_average = @user.entries.where("created_at > ?", 30.days.ago).average(:weight)
+    @twelve_month_average = @user.entries.where("created_at > ?", 12.months.ago).average(:weight)
   end
 end
